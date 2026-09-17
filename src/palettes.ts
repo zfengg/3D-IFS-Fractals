92e7e484c1ca5c252622f7877326480585333f90
Zhou FENG <zfengjoe@outlook.com>
2026-09-17 22:33:56 +0300

    Swap Viridis and Aurora palette positions

--- src/palettes.ts
+++ src/palettes.ts
@@ -2,10 +2,10 @@
  * https://matplotlib.org/stable/gallery/color/colormap_reference.html
  */
 export const palettes:Record<string,string[]>={
- aurora:['#416db8','#54bfb5','#d6ef92'],
+ viridis:['#440154','#3b528b','#21918c','#5ec962','#fde725'],
  ember:['#a43889','#ed7751','#ffe7a1'],
  ocean:['#5145bc','#44a6e6','#b2f8ed'],
- viridis:['#440154','#3b528b','#21918c','#5ec962','#fde725'],
+ aurora:['#416db8','#54bfb5','#d6ef92'],
  plasma:['#0d0887','#7e03a8','#cc4778','#f89540','#f0f921'],
  inferno:['#000004','#57106e','#bc3754','#f98e09','#fcffa4'],
  magma:['#000004','#51127c','#b73779','#fc8961','#fcfdbf'],
